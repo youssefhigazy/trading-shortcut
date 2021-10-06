8119d657a776f73fd0b205e41f4f119f8ec82d2b
Youssef Higazy <yoyohigazy77@gmail.com>
2021-10-06 07:02:04 -0500

    Update the trading-charts component to render the charts based on the provided route

--- src/app/trading-charts/trading-charts.component.spec.ts
+++ src/app/trading-charts/trading-charts.component.spec.ts
@@ -1,25 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { TradingChartsComponent } from './trading-charts.component';
+import { ActivatedRoute, convertToParamMap, Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('TradingChartsComponent', () => {
   let component: TradingChartsComponent;
   let fixture: ComponentFixture<TradingChartsComponent>;
+  let route: ActivatedRoute;
+  let currentRoute: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TradingChartsComponent ]
+      declarations: [ TradingChartsComponent ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      providers: [ ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TradingChartsComponent);
+    route = TestBed.inject(ActivatedRoute);
+    currentRoute = TestBed.inject(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should check if the -What is new?- modal appear once the page is loaded', () => {
+    
+    let modalToggleClassName = 'toggle-modal';
+
+    component.showModal();
+    
+    expect(component.modal.classList.contains(modalToggleClassName)).toBe(true);
+  });
+
+  it('should check if the -What is new?- modal remove once the X button is clicked', () => {
+    let modalToggleClassName = 'toggle-modal';
+
+    component.hideModal();
+
+    expect(component.modal.classList.contains(modalToggleClassName)).toBe(false);
   });
 });
