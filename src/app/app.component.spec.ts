fb2d91b1e1c24f0dc67e6592150d0b302e3b0673
Youssef Higazy <yoyohigazy77@gmail.com>
2021-10-06 07:02:26 -0500

    Update testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('trading-shortcut');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('trading-shortcut app is running!');
-  });
+
 });
